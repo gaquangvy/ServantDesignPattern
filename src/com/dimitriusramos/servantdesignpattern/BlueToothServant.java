600d13b0d968574da72bbc9faf5f990e6a15633c
Ryan Dimitri Ramos <dimitriramos99@gmail.com>
2020-11-17 22:09:08 -0800

    changed turn on bluetooth a little

--- src/com/dimitriusramos/servantdesignpattern/BlueToothServant.java
+++ src/com/dimitriusramos/servantdesignpattern/BlueToothServant.java
@@ -4,8 +4,12 @@ public class BlueToothServant{
 
     public void turnOnBlueTooth(MobileDevice device)
     {
-        device.performStatusCheck();
-        System.out.println("Turning on bluetooth");
+        if(device.performStatusCheck()){
+            System.out.println("Turning on bluetooth");
+        }else{
+            System.out.println("Device failed status check cannot turn on bluetooth");
+        }
+
     }
 }
 
